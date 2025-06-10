c1c2a3c4cc87d995bb906626d3f2ead0f11744ac
shraddha salvi <shraddhasalvi272@gmail.com>
2025-06-10 12:42:10 +0530

    completed imports and export for view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
 
-const index = () => {
+import {
+  HomeView,
+  ToolView,
+  FeatureView,
+  OfferView,
+  FaqView,
+  CreateView,
+  ContactView,
+  AirdropView,
+  DonateView,
+  TokenMetadata,
+} from "../views";
+
+const Home: NextPage = (props) => {
+  const [openCreateModal, setOpenCreateModal] = useState(false);
+  const [openTokenMetaData, setOpenTokenMetaData] = useState(false);
+  const [openContact, setOpenContact] = useState(false);
+  const [openAirdrop, setOpenAirdrop] = useState(false);
+  const [openSendTransaction, setOpenSendTransaction] = useState(false);
   return (
-    <div>index</div>
-  )
-}
-
-export default index
+    <>
+      <Head>
+        <title>Quinty</title>
+        <meta
+          name="Solana token creator"
+          content="Build and create solana token"
+        />
+      </Head>
+      <HomeView setOpenCreateModal={setOpenCreateModal} />
+      <ToolView
+        setOpenAirdrop={setOpenAirdrop}
+        setOpenContact={setOpenContact}
+        setOpenCreateModal={setOpenCreateModal}
+        setOpenSendTransaction={setOpenSendTransaction}
+        setOpenTokenMetaData={setOpenTokenMetaData}
+      />
+      <FeatureView
+        setOpenAirdrop={setOpenAirdrop}
+        setOpenContact={setOpenContact}
+        setOpenCreateModal={setOpenCreateModal}
+        setOpenSendTransaction={setOpenSendTransaction}
+        setOpenTokenMetaData={setOpenTokenMetaData}
+      />
+      <OfferView />
+      <FaqView />
+      {openCreateModal && (
+        <div className="new_loaer relative h-full bg-slate-900">
+          <CreateView setOpenCreateModal={setOpenCreateModal} />
+        </div>
+      )}
+      {openTokenMetaData && (
+        <div className="new_loaer relative h-full bg-slate-900">
+          <TokenMetadata setOpenTokenMetaData={setOpenTokenMetaData} />
+        </div>
+      )}
+      {openTokenMetaData && (
+        <div className="new_loaer relative h-full bg-slate-900">
+          <ContactView setOpenTokenMetaData={setOpenTokenMetaData} />
+        </div>
+      )}
+      {openContact && (
+        <div className="new_loaer relative h-full bg-slate-900">
+          <TokenMetadata setOpenContact={setOpenContact} />
+        </div>
+      )}
+      {openAirdrop && (
+        <div className="new_loaer relative h-full bg-slate-900">
+          <AirdropView setOpenAirdrop={setOpenAirdrop} />
+        </div>
+      )}
+     {openSendTransaction && (
+        <div className="new_loaer relative h-full bg-slate-900">
+          <DonateView setOpenSendTransaction={setOpenSendTransaction} />
+        </div>
+      )}
+    </>
+  );
+};
+export default Home;
